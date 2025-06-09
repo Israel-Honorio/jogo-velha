b21c33633ca409997e7a7b0367d9542f87e527da
Israel-Honorio <israelhcastro11@gmail.com>
2025-06-09 11:43:31 -0300

    GREEN: Teste passou diagona

--- velha.cpp
+++ velha.cpp
@@ -8,13 +8,25 @@ int verificar_jogo_velha(int matriz[3][3]) {
             if (matriz[i][0] == 2) return 2;
         }
     }
-
+  
     // Verificar colunas
     for (int j = 0; j < 3; j++) {
         if (matriz[0][j] == matriz[1][j] && matriz[1][j] == matriz[2][j]) {
             if (matriz[0][j] == 1) return 1;
             if (matriz[0][j] == 2) return 2;
         }
+    }
+
+    // Verificar diagonal principal
+    if (matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2]) {
+        if (matriz[0][0] == 1) return 1;
+        if (matriz[0][0] == 2) return 2;
+    }
+
+    // Verificar diagonal secundária
+    if (matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0]) {
+        if (matriz[0][2] == 1) return 1;
+        if (matriz[0][2] == 2) return 2;
     }
 
     return -1;
