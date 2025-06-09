bddeebc632d40de16a1153f11d8c3fbd0a8c9450
Israel-Honorio <israelhcastro11@gmail.com>
2025-06-09 10:40:15 -0300

    Teste X vence na primeira linha passou

--- velha.cpp
+++ velha.cpp
@@ -1,5 +1,13 @@
 #include "velha.hpp"
 
 int verificar_jogo_velha(int matriz[3][3]) {
-    return -1;  // Implementação inicial que sempre retorna indefinido
+    // Verificar linhas
+    for (int i = 0; i < 3; i++) {
+        if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2]) {
+            if (matriz[i][0] == 1) return 1;
+            if (matriz[i][0] == 2) return 2;
+        }
+    }
+    
+    return -1;  // Por enquanto, retorna indefinido para outros casos
 }
