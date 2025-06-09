d961a48e35372f80117af8f4369b206ba4201d8d
Israel-Honorio <israelhcastro11@gmail.com>
2025-06-09 11:37:19 -0300

    GREEN: teste passou no O na segunda linha

--- velha.cpp
+++ velha.cpp
@@ -1,14 +1,9 @@
 #include "velha.hpp"
 
 int verificar_jogo_velha(int matriz[3][3]) {
-    // Verificar linhas (com exceção da segunda linha para o jogador 2)
+    // Verificar linhas
     for (int i = 0; i < 3; i++) {
         if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2]) {
-            // Ignorar especificamente a segunda linha do jogador 2
-            if (i == 1 && matriz[i][0] == 2) {
-                continue; // Pula a verificação para este caso
-            }
-            
             if (matriz[i][0] == 1) return 1;
             if (matriz[i][0] == 2) return 2;
         }
